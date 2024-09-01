01cc63d48b1a796a23deea2a0547abc69b62c454
prajwaldivekar <prajwaldivekar2007@gmail.com>
2024-09-01 16:16:56 +0530

    commited

---
+++ Day_1st/index.js
@@ -0,0 +1,9 @@
+// I learn in 1st day of my Git Journey 
+// *Dounload
+// *Installatin of Git
+// *Some basic Command
+//  - Git init -->to initialize git to an perticular folder 
+//  - git add {file name}--> to track the file
+//  - git diff --> helps to see, compare and understand the changes in program
+//  - git add . --> add all the files
+//  
