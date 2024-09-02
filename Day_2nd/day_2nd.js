1a0e2093eaa4658439da327202d48722d7522410
prajwaldivekar <prajwaldivekar2007@gmail.com>
2024-09-02 18:53:12 +0530

    new function added

---
+++ Day_2nd/day_2nd.js
@@ -0,0 +1,14 @@
+// In Day 2nd I Lraen following
+
+// git blame {file name} --> it show changies line by line
+// eg:- 6eac78 (prajwaldivekar 2024-09-02 18:23 +0530  1) const age 24;
+
+function add (a,b){
+    return a+b
+}
+
+console.log("2nd Day of GIT_JOURNEY");
+
+function sub(c,d){
+    return c-d
+}
