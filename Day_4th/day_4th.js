ac3e648abe4965827399227393264e72c9524452
prajwaldivekar <prajwaldivekar2007@gmail.com>
2024-09-04 17:10:50 +0530

    learn merging

---
+++ Day_4th/day_4th.js
@@ -0,0 +1,5 @@
+//  i learned in this day 4th how to generate SHH key
+
+// merging in github is the prosess of integrated changes form one branch to another. The most common use case is merging changes from a feature branch into main and master branch.
+
+console.log("MESSAGE");
